d1ec78135dcc3470e9c774b03f5aaf8ab4e5c85f
vk8958 <vk82851781@gmail.com>
2022-06-19 00:44:46 +0530

    push

--- Color Flipper/index.js
+++ Color Flipper/index.js
@@ -9,11 +9,20 @@ console.log(btn[0])
 console.log(colortext[0].innerHTML)
 
 
-const arr=["#FF0000","#00FF00","FFFFFF","0000FF","#FF0000","#00FF00","FFFFFF","0000FF","#FF0000","#00FF00","FFFFFF","0000FF"];
+const arr=["#C0C0C0","#800000","#00FFFF","#808000","#FFFFF00","#00FF00","#FFFFFF","#000000","#FF0000","#0000FF"];
 
-//console.log(arr[Math.floor(Math.random()*10)])
+
 
 btn[0].addEventListener("click",function(){
+    console.log(arr[Math.floor(Math.random()*10)])
     const color=arr[Math.floor(Math.random()*10)];
     background[0].style.background=color;
 })
+
+setInterval(function(){
+    
+        console.log(arr[Math.floor(Math.random()*10)])
+        const color=arr[Math.floor(Math.random()*10)];
+        background[0].style.background=color;
+    
+},1);
